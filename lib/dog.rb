e3b8262db04476e812ad1e8ae2e8a39c0f608516
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-30 02:55:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,18 +24,3 @@ end
 
 natalie = Dog.new("Natalie")
 Dog.all
-
-    describe ".clear_all" do
-      it "is a class method that empties the @@all array of all existing dogs" do
-        Dog.clear_all
-        expect(Dog.class_variable_get(:@@all)).to match([]  )
-      end
-    end
-  
-    describe ".print_all" do
-      it "is a class method that puts out the name of each dog to the terminal" do
-        expect{Dog.print_all}.to output("Pluto\nFido\nMaddy\n").to_stdout
-      end
-    end
-  end
-  
